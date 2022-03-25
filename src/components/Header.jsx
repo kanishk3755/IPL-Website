3e160c04c58680dec62dc6e193eb5e53f5ceab20
Kanishk Khurana <76431947+kanishk3755@users.noreply.github.com>
2022-03-25 22:48:00 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <a href="https://www.iplt20.com/matches/schedule/men">
+          <img
+            src="https://www.iplt20.com/assets/images/ipl-logo-new-old.png"
+            className="ipl-logo"
+            alt="IPL logo"
+          />
+        </a>
+      </nav>
+      <div className="heading">Schedule</div>
+    </>
+  )
+}
+
+export default Header
